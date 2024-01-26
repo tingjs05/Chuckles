da7bdcd72a6bc1ef903d39603e456be47a34cdc5
ultr42 <ultr42@skiff.com>
2024-01-26 23:21:59 +0800

    wonky speech to text

--- Assets/Scripts/Dictation/SpeechToText.cs
+++ Assets/Scripts/Dictation/SpeechToText.cs
@@ -1,7 +1,31 @@
-﻿namespace Dictation
+﻿using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+namespace Dictation
 {
-    public class SpeechToText
+    public class SpeechToText : MonoBehaviour
     {
-        
+        public string[] keywords = new string[] {"ha"};
+        public ConfidenceLevel confidence = ConfidenceLevel.Medium;
+        private KeywordRecognizer recognizer;
+
+        private void Start()
+        {
+            recognizer = new KeywordRecognizer(keywords, confidence);
+            recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+            recognizer.Start();
+        }
+        private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+        {
+            Debug.Log($"Phrase {args.text} recognized with confidence: {args.confidence}");
+        }
+        private void OnApplicationQuit()
+        {
+            if (recognizer != null && recognizer.IsRunning)
+            {
+                recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+                recognizer.Stop();
+            }
+        }
     }
 }
