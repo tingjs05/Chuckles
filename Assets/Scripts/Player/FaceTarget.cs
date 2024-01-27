a0fe8125d80e43a73ea57fd3701beb9596c239ed
ultr42 <ultr42@skiff.com>
2024-01-27 16:01:41 +0800

    always face camera script

---
+++ Assets/Scripts/Player/FaceTarget.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class FaceTarget : MonoBehaviour
+    {
+        public Transform cameraTransform;
+        
+        private void Update()
+        {
+            transform.LookAt(cameraTransform);
+        }
+    }
+}
