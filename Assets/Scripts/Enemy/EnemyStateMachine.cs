2aa443391b0f45fd221d88f43981cc892c0ea59f
Samuel Ting <tingjs05@gmail.com>
2024-01-27 18:13:32 +0800

    added clown states

--- Assets/Scripts/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/Enemy/EnemyStateMachine.cs
@@ -7,9 +7,15 @@ public class EnemyStateMachine : MonoBehaviour
     public EnemyBaseState State { get; private set; }
 
     // states
+    public EnemyIdleState Idle { get; private set; } = new EnemyIdleState();
+    public EnemyAlertState Alert { get; private set; } = new EnemyAlertState();
+    public EnemyChaseState Chase { get; private set; } = new EnemyChaseState();
+
+    [field: SerializeField] public EnemyBaseState[] EnemyActionStates;
 
     void Start()
     {
+        State = Idle;
         State.OnEnter(this);
     }
 
---
+++ Assets/Scripts/Enemy/States/EnemyChaseState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChaseState : EnemyBaseState
+{
+    public override void OnEnter(EnemyStateMachine enemy)
+    {
+        
+    }
+
+    public override void OnUpdate(EnemyStateMachine enemy)
+    {
+
+    }
+
+    public override void OnExit(EnemyStateMachine enemy)
+    {
+        
+    }
+}
