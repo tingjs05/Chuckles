1a4f1cb3a2a6d52c7bd6c3e8f728ec7f3820a259
Samuel Ting <tingjs05@gmail.com>
2024-01-27 21:00:41 +0800

    added patrol state

--- Assets/Scripts/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/Enemy/EnemyStateMachine.cs
@@ -5,8 +5,23 @@ using UnityEngine.AI;
 
 namespace Enemy
 {
+    [RequireComponent(typeof(NavMeshAgent))]
     public class EnemyStateMachine : MonoBehaviour
     {
+        // inspector elements
+        [Header("Movement")]
+        public float walkSpeed = 10f;
+        public float sprintSpeed = 25f;
+
+        [Header("Range Checks")]
+        public float patrolRange = 25f;
+        public float alertRange = 15f;
+        public float detectionRange = 10f;
+
+        [Header("Player Information")]
+        public LayerMask playerMask;
+
+        // current state of enemy
         public EnemyBaseState State { get; private set; }
 
         // states
@@ -33,6 +48,15 @@ namespace Enemy
         void Update()
         {
             State.OnUpdate(this);
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawWireSphere(transform.position, patrolRange);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, alertRange);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, detectionRange);
         }
 
         public void SwitchState(EnemyBaseState state)
