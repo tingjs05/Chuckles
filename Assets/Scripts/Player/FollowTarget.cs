16ddba5e0e32b58f7372d3d52e2990a6f8eca6d8
ultr42 <ultr42@skiff.com>
2024-01-27 13:56:32 +0800

    added follow target for player

---
+++ Assets/Scripts/Player/FollowTarget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class FollowTarget : MonoBehaviour
+    {
+        public float smoothTime = 0.5f;
+        public Transform target;
+        public Vector3 offset;
+        private Vector3 velocity = Vector3.zero;
+
+        private void Update()
+        {
+            Vector3 desiredPosition = target.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime,
+                Mathf.Infinity, deltaTime: Time.deltaTime);
+        }
+    }
+}
