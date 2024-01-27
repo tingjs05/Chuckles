7bf2f843cf7c538f626c732b0324d0717f94acf8
glenn-chann <glenn.chan1212@gmail.com>
2024-01-28 03:35:24 +0800

    error handling

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -13,30 +13,41 @@ public class Water : MonoBehaviour
 
     float displacementMultiplier;
 
-    bool isInWater;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Player")
+        Movement move = other.GetComponent<Movement>();
+        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
         {
             splash.Play(this);
-            other.GetComponent<Movement>().speed /= 2;
+            if (move != null )
+            {
+                move.speed /= 2;
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        Movement move = other.GetComponent<Movement>();
         if (other.gameObject.name == "Player")
         {
-            other.GetComponent<Movement>().speed *= 2;
+            if (move != null)
+            {
+                move.speed *= 2;
+            }
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
         if (other.gameObject.transform.position.y < waterHeight)
         {
             displacementMultiplier = Mathf.Clamp01((waterHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
-            other.GetComponent<Rigidbody>().AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+            if (rb != null)
+            {
+                rb.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+            }
         }
     }
 }
