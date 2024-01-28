144abc4814c2c2cc109096dcbc43ac55d470c805
ultr42 <ultr42@skiff.com>
2024-01-28 13:17:23 +0800

    adding daycycle

---
+++ Assets/Scripts/DayCycle.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+public class DayCycle : MonoBehaviour
+{
+    public Light sun;
+    public float secondsInFullDay = 120f;
+
+    public float dayIntensity = 1f;
+    public float nightIntensity = 0.1f;
+
+    [Range(0, 1)]
+    public float currentTimeNormalised = 0f;
+
+    public float timeMultiplier = 1f;
+
+    public bool isDay => Math.Abs(currentTimeNormalised - 1f) < 0.01f;
+
+
+    public event Action DayEnded;
+
+    [EasyButtons.Button]
+    public void ResetDay() { currentTimeNormalised = 0; }
+
+    public void SetTime(float time)
+    {
+        currentTimeNormalised = Mathf.Clamp01(time);
+        sun.intensity = Mathf.Lerp(nightIntensity, dayIntensity, currentTimeNormalised);
+        if (isDay) DayEnded?.Invoke();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDay) return;
+        SetTime(currentTimeNormalised + (Time.deltaTime / secondsInFullDay) * timeMultiplier);
+    }
+}
