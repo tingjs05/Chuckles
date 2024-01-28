f7aadd4b42e676a94c2a6cfb7d83953f536e9385
sirpotatosalad <timothy.a.anoso@gmail.com>
2024-01-28 14:02:33 +0800

    Finalised Main Menu and Lose

---
+++ Assets/Scripts/UI/LoseUI.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseUI : MonoBehaviour
+{
+    [Header("UI Objects")]
+    public GameObject caughtUI;
+    public GameObject loseUI;
+    public GameObject clown;
+    public GameObject laughingUIPrefab;
+    public GameObject letsPlayText;
+    public Transform laughParent;
+    public GameObject ambientNoise;
+
+    [Header("SFX")]
+    private AudioSource audioSource;
+    public AudioClip laughSound;
+
+
+    [Header("Instantiation parameters")]
+    public int rowCount = 16;
+    public int columnCount = 10;
+    public float spacingX = 110f;
+    public float spacingY = 110f;
+
+    private const float displayDelay = 0.0001f;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        caughtUI.SetActive(true);
+        loseUI.SetActive(false);
+
+        audioSource = GetComponent<AudioSource>();
+
+        clown.SetActive(false);
+        letsPlayText.SetActive(false);
+
+
+        StartCoroutine(InstantiateUIPrefabs());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator InstantiateUIPrefabs()
+    {
+        
+        yield return new WaitForSeconds(2.5f);
+
+        ambientNoise.SetActive(false);
+        clown.SetActive(true);
+
+        yield return new WaitForSeconds(1.5f);
+
+        Vector3 startingPos = Vector3.zero;
+
+        //play one shot sound LOL
+        audioSource.PlayOneShot(laughSound);
+
+        for (int row = 0; row < rowCount; row++)
+        {
+            for (int col = 0; col < columnCount; col++)
+            {
+                Vector3 position = startingPos + new Vector3(col * spacingX, row * spacingY, 0f);
+
+
+                GameObject instantiatedUI = Instantiate(laughingUIPrefab, position, Quaternion.identity);
+
+                instantiatedUI.transform.SetParent(laughParent);
+
+                yield return new WaitForSeconds(displayDelay);
+            }
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+
+        letsPlayText.SetActive(true);
+
+        yield return new WaitForSeconds(3f);
+
+        caughtUI.SetActive(false);
+
+        yield return new WaitForSeconds(1.5f);
+
+        loseUI.SetActive(true);
+
+    }
+
+    public void OnClickRestart()
+    {
+        Debug.Log("Restarting Level");
+        SceneManager.LoadScene("GameLevel");
+    }
+
+    public void OnClickMainMenu()
+    {
+        Debug.Log("Returning to Main Menu");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
